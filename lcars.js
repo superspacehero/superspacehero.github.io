e5e7638eb964052a8399adc95e7fd13f3f773fe1
Asher Pinson <ashpinson@gmail.com>
2022-07-13 20:35:02 -0400

    Work on button Easter Eggs begins

--- lcars.js
+++ lcars.js
@@ -1,19 +1,25 @@
-document.addEventListener("touchstart", function() {},false);
+// document.addEventListener("touchstart", function() {},false);
 
-$(window).scroll(function() {
-    var height = $(window).scrollTop();
-    if (height > 100) {
-        $('.scroll-top a').fadeIn();
-    } else {
-        $('.scroll-top a').fadeOut();
-    }
-});
+// $(window).scroll(function() {
+//     var height = $(window).scrollTop();
+//     if (height > 100) {
+//         $('.scroll-top a').fadeIn();
+//     } else {
+//         $('.scroll-top a').fadeOut();
+//     }
+// });
 
-$(document).ready(function() {
-    $("#scroll-top").click(function(event) {
-        event.preventDefault();
-        $("html, body").animate({ scrollTop: 0 }, "slow");
-        return false;
-    });
+// $(document).ready(function() {
+//     $("#scroll-top").click(function(event) {
+//         event.preventDefault();
+//         $("html, body").animate({ scrollTop: 0 }, "slow");
+//         return false;
+//     });
 
-});
+// });
+
+function playsound() {
+    var mysound = document.getElementById("mysound");
+    mysound.autoplay = 'true';
+    mysound.load();
+}
